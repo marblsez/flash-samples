f3385aeec861ad4c6c0aa5acd25aa7952a890292
sez <e0725352@student.tuwien.ac.at>
2018-04-26 01:51:37 +0200

    init

---
+++ src/test/java/org/flash/samples/AppTest.java
@@ -0,0 +1,16 @@
+package org.flash.samples;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+	@Test
+	public void testOne() {
+		Assert.assertTrue(true);
+	}
+
+	@Test
+	public void testTwo() {
+		Assert.assertTrue(true);
+	}
+}
